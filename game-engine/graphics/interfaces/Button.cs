88346b75b7f03302cc5024de6068b7c82d5b881e
hczaja <hubert_czaja@o2.pl>
2024-03-02 19:07:39 +0100

    HE00205 add simple button and class responsible for extension methods

--- game-engine/graphics/interfaces/Button.cs
+++ game-engine/graphics/interfaces/Button.cs
@@ -4,8 +4,12 @@ namespace game_engine.graphics.interfaces;
 
 public class Button : IDrawable
 {
+    public RectangleShape Shape { get; }
+
+    public Button(RectangleShape shape) => Shape = shape;
+
     public void Draw(RenderTarget render)
     {
-        throw new NotImplementedException();
+        render.Draw(Shape);
     }
 }
---
+++ game-engine/system/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using game_engine.events.input;
+using SFML.Graphics;
+
+namespace game_engine.system;
+
+public static class Extensions
+{
+    public static bool IsMouseEventRaisedIn(this FloatRect rectangle, MouseEvent @event) =>
+        rectangle.Left < @event.X && @event.X < rectangle.Left + rectangle.Width &&
+        rectangle.Top < @event.Y && @event.Y < rectangle.Top + rectangle.Height;
+}
