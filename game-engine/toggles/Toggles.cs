2252e0eea9aa2d074ee96efaf13a8194ea76ad4d
hczaja <hubert_czaja@o2.pl>
2024-03-02 18:34:32 +0100

    HE00202 add toggles mechanism

---
+++ game-engine/toggles/Toggles.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace game_engine.toggles;
+
+public static class Toggles
+{
+    public static bool DrawCollisions { get; private set; }
+
+    public static void Set(string name, bool value)
+    {
+        var property = typeof(Toggles).GetProperty(name, BindingFlags.Public | BindingFlags.Static);
+        if (property is not null)
+        {
+            property.SetValue(null, value);
+        }
+    }
+}
