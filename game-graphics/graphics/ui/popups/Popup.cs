e7283cdd02fd30bc88357d0c852f0c90392b5fbb
hczaja <hubert_czaja@o2.pl>
2024-03-30 16:37:42 +0100

    HE03001 introduction to popups

---
+++ game-graphics/graphics/ui/popups/Popup.cs
@@ -0,0 +1,56 @@
+﻿using game_engine.events;
+using game_engine.events.input;
+using game_engine.system;
+using SFML.Graphics;
+using SFML.System;
+
+namespace game_graphics.graphics.ui.popups;
+
+abstract class Popup
+{
+    IEnumerable<PopupItem> Items { get; }
+
+    protected Popup()
+    {
+        
+    }
+}
+
+class SingleOptionPopup : Popup
+{
+
+}
+
+class TwoOptionsPopup : Popup
+{
+
+}
+
+class TripleOptionsPopup : Popup
+{ 
+}
+
+
+class PopupItem : RectangleShape, IEventHandler<MouseEvent>
+{
+    private Action Callback { get; }
+    private FloatRect Boundaries { get; }
+
+
+    public PopupItem(Vector2f size, Vector2f position, Texture texture, Action callback)
+    {
+        Size = size;
+        Position = position;
+        Texture = texture;
+        Callback = callback;
+        Boundaries = GetGlobalBounds();
+    }
+
+    public void Handle(MouseEvent @event)
+    {
+        if (Boundaries.IsMouseEventRaisedInRectangle(@event) && @event.Type == MouseEventType.Pressed)
+        {
+            Callback();
+        }
+    }
+}
